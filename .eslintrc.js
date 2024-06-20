d754fa284c5a134a315c0eecdd9d3310c7ed39c4
주현정 <61533589+joooo24@users.noreply.github.com>
2024-06-20 13:16:41 +0900

    chore: ESLint, Prettier 설정

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint', 'react', 'prettier'],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
+        'prettier/@typescript-eslint',
+        'plugin:prettier/recommended'
+    ],
+    settings: {
+        react: {
+            version: 'detect'
+        }
+    },
+    rules: {
+        'no-var': 'error', // var 사용 금지
+        'no-multiple-empty-lines': 'error', // 여러줄 공백 금지
+        // 'no-console': ['error', {allow: ['warn', 'error', 'info']}], // console.log 금지, 배포 때 해당 규칙 활성화
+        eqeqeq: 'error', // 엄격한 동등 연산자 사용 (===, !==)
+        'react/jsx-pascal-case': 'error', // 컴포넌트 이름 pascal-Case 사용
+        'react/jsx-key': 'error', // 반복문 내에서 key 사용
+        'dot-notation': 'error', // 객체 접근 시 대괄호 표기법 금지, 점 표기법 사용
+        'linebreak-style': 0, // 줄바꿈 스타일에 대한 규칙 비활성화 (운영체제간 줄바꿈 방식이 다름에 따른 문제 방지)
+        'no-unused-vars': 'off', // 사용되지 않는 변수 사용 금지
+        'no-useless-catch': 'off', // try, catch 비활성화
+        'prettier/prettier': [
+            'error',
+            {
+                endOfLine: 'auto' // Prettier의 규칙을 ESlint에 통합, 줄바꿈 방식에 대해 자동으로 설정
+            }
+        ]
+    }
+};
---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    printWidth: 120,
+    tabWidth: 4,
+    trailingComma: 'es6',
+    semi: true, // 코드의 끝에 세미콜론 포함
+    singleQuote: true, // 문자열을 표시할 때 작은 따옴표 사용 ('')
+    jsxSingleQuote: true, // JSX 속성 내에서 문자열을 표시할 때 작은 따옴표 사용 ('')
+    trailingComma: 'none', // 객체의 마지막 요소에 쉼표 사용 안함
+    bracketSpacing: true, // 객체의 양 끝에 공백 1칸 표시
+    bracketSameLine: true, // HTML에서 닫는 태그를 같은 줄에 유지
+    jsxBracketSameLine: true, // JSX 내의 HTML에서 닫는 태그를 같은 줄에 유지
+    arrowParens: 'always', // 함수 사용 시 파라미터가 1개일 때도 괄호를 사용
+    quoteProps: 'as-needed' // 객체의 key 이름을 따옴표로 감쌀지 여부 (X)
+};
