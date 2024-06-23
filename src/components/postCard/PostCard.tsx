4e5a1612da3e23321e1d4167a383983cc20cf393
claire <61533589+joooo24@users.noreply.github.com>
2024-06-24 00:23:11 +0900

    feat: 포스트카드/컨테이너 형 지정

--- src/components/postCard/PostCard.tsx
+++ src/components/postCard/PostCard.tsx
@@ -2,12 +2,27 @@ import React from 'react';
 import PostImage from '../postImage/PostImage';
 import './postCard.scss';
 
+interface User {
+    _id: string;
+    name: string;
+    profileImage: string;
+}
+
+interface Post {
+    _id: string;
+    userId: User;
+    title: string;
+    content: string;
+    images: string[];
+    category: string;
+    tags: string[];
+    likeCount: number;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
 interface PostCardProps {
-    post: {
-        title: string;
-        content: string;
-        images: string[];
-    };
+    post: Post;
 }
 
 const PostCard: React.FC<PostCardProps> = ({ post }) => {
@@ -15,8 +30,8 @@ const PostCard: React.FC<PostCardProps> = ({ post }) => {
         <div className='post-card'>
             <h2>{post.title}</h2>
             <p>{post.content}</p>
-            {post.images.map((_image, index) => (
-                <PostImage key={index} src={''} alt={''} />
+            {post.images.map((image, index) => (
+                <PostImage key={index} src={image} alt={`Image ${index}`} />
             ))}
         </div>
     );
--- src/components/postCard/PostCardContainer.tsx
+++ src/components/postCard/PostCardContainer.tsx
@@ -5,6 +5,27 @@ import { IoIosMore } from 'react-icons/io';
 import { useGetPosts } from './../../hooks/usePostHook';
 import './postCard.scss';
 
+import { AxiosResponse } from 'axios';
+
+interface User {
+    _id: string;
+    name: string;
+    profileImage: string;
+}
+
+interface Post {
+    _id: string;
+    userId: User;
+    title: string;
+    content: string;
+    images: string[];
+    category: string;
+    tags: string[];
+    likeCount: number;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
 const PostCardContainer: React.FC = () => {
     const { data, isLoading, isError } = useGetPosts();
 
@@ -18,15 +39,13 @@ const PostCardContainer: React.FC = () => {
 
     return (
         <div className='post-card-container'>
-            {data.data.map((post: any) => (
+            {data.data.map((post: AxiosResponse<Post[]>['data'][0]) => (
                 <div key={post._id} className='post-card'>
                     <div className='post-action'>
                         <ProfileImage userId={post.userId._id} />
                         <ul className='post-info'>
                             <li className='post-id'>{post.userId.name}</li>
-                            <li className='post-date'>날짜</li>
-                            <li className='post-location'>위치</li>
-                            {/* <li className='post-date'>{new Date(post.createdAt).toLocaleDateString()}</li> */}
+                            <li className='post-date'>{new Date(post.createdAt).toLocaleDateString()}</li>
                             {/* <li className='post-location'>{post.location}</li> */}
                         </ul>
                         <IoIosMore />
