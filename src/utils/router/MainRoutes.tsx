b842de4df094bfdf37496a8737c47a0dffae6c20
주현정 <61533589+joooo24@users.noreply.github.com>
2024-06-26 15:03:59 +0900

    chore: MyFeedPage -> FeedPage

--- src/utils/router/MainRoutes.tsx
+++ src/utils/router/MainRoutes.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import LoginPage from '../../pages/user/LoginPage';
 import RegisterPage from '../../pages/user/RegisterPage';
-import MyFeedPage from '../../pages/feed/MyFeedPage';
+import FeedPage from '../../pages/feed/FeedPage';
 import FeedDetailPage from '../../pages/feed/FeedDetailPage';
 import MainPage from '../../pages/MainPage';
 
@@ -10,7 +10,7 @@ const MainRoutes: React.FC = () => {
     return (
         <Routes>
             <Route path='/' element={<MainPage />} />
-            <Route path='/feed/:id' element={<MyFeedPage />} />
+            <Route path='/feed/:id' element={<FeedPage />} />
             <Route path='/feed/:id/:id' element={<FeedDetailPage />} />
             <Route path='/user/login' element={<LoginPage />} />
             <Route path='/user/register' element={<RegisterPage />} />
