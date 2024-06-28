f3327c8b941e5e78af5657e107c96010673f1264
LAPTOP-9618V2DO\POINT-I <agratos318@naver.com>
2024-06-28 15:58:28 +0900

    textarea css 완료

--- src/components/comment/CommentForm.tsx
+++ src/components/comment/CommentForm.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { FaCircleUser } from 'react-icons/fa6';
 import './comment.scss';
@@ -13,41 +13,45 @@ interface CommentFormProps {
 }
 
 const CommentForm: React.FC<CommentFormProps> = ({ onAddComment }) => {
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-        reset,
-        watch
-    } = useForm<ICommentFormInput>();
+    const { register, handleSubmit, reset, watch } = useForm<ICommentFormInput>();
 
     const commentValue = watch('comment', '');
+
+    useEffect(() => {
+        autoResizeTextarea();
+    }, [commentValue]);
 
     const onSubmit: SubmitHandler<ICommentFormInput> = (data) => {
         onAddComment(data.comment);
         reset(); // 제출 후 폼 초기화
     };
 
+    const autoResizeTextarea = () => {
+        const textarea: HTMLTextAreaElement = document.getElementById('comment') as HTMLTextAreaElement;
+
+        if (textarea) {
+            textarea.style.height = '40px';
+            const height = textarea.scrollHeight; // 높이
+            if (height + 8 >= 88) return (textarea.style.height = `${88}px`);
+            textarea.style.height = `${height + 8}px`;
+        }
+    };
+
     return (
-        <form onSubmit={handleSubmit(onSubmit)} className='form-container form-container-comment'>
-            <section className='comment-profile-section'>
-                <div className='comment-none-profile'>
-                    <FaCircleUser size={36} color={'#e8e8e8'} />
-                </div>
-            </section>
-            <div className='comment-textarea'>
+        <form onSubmit={handleSubmit(onSubmit)} className='form-container form-wrapper'>
+            <div className='form-inner-section'>
+                <FaCircleUser size={40} color={'#e8e8e8'} />
                 <textarea
                     id='comment'
                     {...register('comment', { required: '댓글이 입력되지 않았습니다.' })}
                     placeholder='댓글 달기...'
                 />
-                {errors.comment && <p>{errors.comment.message}</p>}
+                {commentValue && (
+                    <button type='submit' className='form-textarea-submit'>
+                        게시
+                    </button>
+                )}
             </div>
-            {commentValue && (
-                <button type='submit' className='btn btn-submit'>
-                    게시
-                </button>
-            )}
         </form>
     );
 };
