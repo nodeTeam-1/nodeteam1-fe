5a79c93df44b31d8d6248edfbf340afafb7ccf06
claire <61533589+joooo24@users.noreply.github.com>
2024-06-30 17:57:46 +0900

    chore: 에러메시지 수정

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -9,7 +9,6 @@ const NotFoundPage: React.FC<NotFoundPageProps> = ({ message }) => {
     return (
         <div className='page-container error-page'>
             <img src='/404.png' alt='error-page' />
-            <h1>404 - Page Not Found</h1>
             <p>{message || 'The page you are looking for does not exist.'}</p>
         </div>
     );
